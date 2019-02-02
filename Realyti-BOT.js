67340d44b760db2d2f9493a27a32c033d9c34173
RedTazz28 <47223194+RedTazz28@users.noreply.github.com>
2019-02-02 10:53:25 +0100

    Update Realyti-BOT.js

--- Realyti-BOT.js
+++ Realyti-BOT.js
@@ -1,36 +1,4 @@
-const Discord = require('discord.js');
 
-const config = require('./config.json');
-const cmds = require('./commands.js');
-const music = require('./music.js');
-const tool = require('./tool.js');
-
-const prompt = require('prompt');
-const colors = require('colors');
-prompt.message = '';
-prompt.delimiter = '';
-
-
-const client = new Discord.Client();
-
-var prefix = "*";
-
-client.login();
-
-client.on("ready", () => {
-    console.log("je suis pret !");
-    client.user.setGame("s'ameliorer !!!help");
-});
-
-client.on('message', message => { 
-
-    if (message.content === "!!!create"){
-        message.reply("bonjoure je suis le BOT cree par Redtazz28");
-        console.log('Le BOT dis qui est son createur');
-    }
-
-    if (message.content === "!!!site"){
-        message.reply("https://*****.000webhostapp.com");
         console.log('yo poto on est la');
     } 
 
